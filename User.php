a3fae08338b160b43a8e0fb9551e83635bdbf289
Galbur Sergey <gal_sergey@ukr.net>
2020-08-24 23:34:32 +0300

    created a class

---
+++ User.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace OnixPHPInternshipSergeyGalbur;
+
+/**
+ * Description of User
+ *
+ * @author gal_s
+ */
+class User {
+    private $name;
+    private $balance;
+    public function __construct($name = '', $balance = '0') {
+        $this->name = $name;
+        $this->balance = $balance;
+    }
+    function getName(){
+        return $this->name;
+    }
+    function getBalance(){
+        return $this->balance;
+    }
+
+    public function printStatus() {
+        echo "У пользователя $this->name  сейчас на счету $this->balance грн.<br>";
+    }
+    public function giveMoney($amount, $user) {
+        if($this->balance > $amount){
+            $this->balance -= $amount;
+            $user->takeMoney($amount);
+        }else{
+            $this->balance = 0;
+            $user->takeMoney($this->balance);
+            
+        }
+        
+    }
+    private function takeMoney($amount){
+        $this->balance += $amount;
+    }
+    public function getProp($prop) {
+        return $this->$prop;
+    }
+}
