52b1ebf07ec1b63626bc91bb8ac238aad95c49d0
Galbur Sergey <gal_sergey@ukr.net>
2020-08-24 23:36:36 +0300

    tested the class User

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace OnixPHPInternshipSergeyGalbur;
 
 
+require_once 'User.php';
+$user1 = new User('Vasia', 100);
+$user2 = new User('Olga', 100);
+;
+$user1->printStatus();
+$user2->printStatus();
+
+echo '<br>';
+echo $user1->getProp('name')." дал ".$user2->getProp('name')." 50 грн.<br><br>";
+$user1->giveMoney(50,$user2);
+$user1->printStatus();
+$user2->printStatus();
+echo '<br>';
+echo $user1->getProp('name')." должен ".$user2->getProp('name')." еще 100грн. грн.<br><br>";
+$user1->giveMoney(100,$user2);
+$user1->printStatus();
+$user2->printStatus();
