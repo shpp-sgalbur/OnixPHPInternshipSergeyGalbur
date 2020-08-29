3b757cbd3a049d9d04ca961debd94db2a2e44e41
Galbur Sergey <gal_sergey@ukr.net>
2020-08-29 16:59:09 +0300

    Replaced the print Status method with the magic __toString method.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ namespace lesson1;
 
 
 require_once 'lesson1/User.php';
+
 $user1 = new User('Vasia', 100);
 $user2 = new User('Olga', 100);
 ;
@@ -27,18 +28,18 @@ namespace lesson2;
 require_once 'lesson2/User.php';
 $user1 = new User('Vasia', 100);
 $user2 = new User('Olga', 100);
-;
-$user1->printStatus();
-$user2->printStatus();
+echo "==================== <br>";
+echo $user1;
+echo $user2;
 
 echo '<br>';
 echo $user1->getProp('name')." дал ".$user2->getProp('name')." 50 грн.<br><br>";
 $user1->giveMoney(50,$user2);
-$user1->printStatus();
-$user2->printStatus();
+echo $user1;
+echo  $user2;
 
 echo '<br>';
 echo $user1->getProp('name')." должен ".$user2->getProp('name')." еще 100грн. грн.<br><br>";
 $user1->giveMoney(100,$user2);
-$user1->printStatus();
-$user2->printStatus();
+echo $user1;
+echo $user2;
